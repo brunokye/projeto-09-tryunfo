923bb623bf7282f6780c29333763caa394e1597a
Bruno Ferreira <contatobrunokye@gmail.com>
2022-10-26 15:07:37 -0300

    Adiciona requisito 2

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,68 +2,102 @@ import React, { Component } from 'react';
 
 export default class Form extends Component {
   render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      hasTrunfo,
+      isSaveButtonDisabled,
+      onInputChange,
+      onSaveButtonClick,
+    } = this.props;
+
     return (
       <form>
         <label htmlFor="cardName">
           Nome
           <input
+            id="name-input"
             name="cardName"
             data-testid="name-input"
             type="text"
+            value={ cardName }
+            onChange={ onInputChange }
           />
         </label>
 
         <label htmlFor="description">
           Descrição
           <textarea
+            id="description-input"
             name="description"
             data-testid="description-input"
-            cols="30"
-            rows="10"
+            value={ cardDescription }
+            onChange={ onInputChange }
           />
         </label>
 
-        <label htmlFor="attack">
+        <label htmlFor="attr1-input">
           Ataque
           <input
+            id="attr1-input"
             name="attack"
             data-testid="attr1-input"
             type="number"
+            value={ cardAttr1 }
+            onChange={ onInputChange }
           />
         </label>
 
-        <label htmlFor="defense">
+        <label htmlFor="attr2-input">
           Defesa
           <input
+            id="attr2-input"
             name="defense"
             data-testid="attr2-input"
             type="number"
+            value={ cardAttr2 }
+            onChange={ onInputChange }
           />
         </label>
 
-        <label htmlFor="speed">
+        <label htmlFor="attr3-input">
           Velocidade
           <input
+            id="attr3-input"
             name="speed"
             data-testid="attr3-input"
             type="number"
+            value={ cardAttr3 }
+            onChange={ onInputChange }
           />
         </label>
 
-        <label htmlFor="image">
+        <label htmlFor="image-input">
           Imagem
           <input
+            id="image-input"
             name="image"
             data-testid="image-input"
             type="text"
+            value={ cardImage }
+            onChange={ onInputChange }
           />
         </label>
 
-        <label htmlFor="rarity">
+        <label htmlFor="rare-input">
           Raridade
           <select
+            id="rare-input"
             name="rarity"
             data-testid="rare-input"
+            value={ cardRare }
+            onChange={ onInputChange }
           >
             <option value="normal">Normal</option>
             <option value="raro">Raro</option>
@@ -71,18 +105,24 @@ export default class Form extends Component {
           </select>
         </label>
 
-        <label htmlFor="cardSuper">
+        <label htmlFor="trunfo-input">
           Super Trunfo
           <input
+            id="trunfo-input"
             name="cardSuper"
             data-testid="trunfo-input"
             type="checkbox"
+            checked={ cardTrunfo }
+            onChange={ onInputChange }
           />
         </label>
 
         <button
+          id="save-button"
           data-testid="save-button"
           type="button"
+          disabled={ isSaveButtonDisabled }
+          onClick={ onSaveButtonClick }
         >
           Salvar
         </button>
